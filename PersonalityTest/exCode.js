9f54091bb00283a745b41dcd4a1372ec1a638cbd
Judy Zhang <judyzhang@MacBook-Pro-2.local>
2020-04-04 13:27:03 -0700

    fix character selection and passing character from scene to scene

--- PersonalityTest/exCode.js
+++ PersonalityTest/exCode.js
@@ -1,6 +1,13 @@
-﻿class OpeningScreen extends Phaser.Scene { /******** OPENING SCREEN ********/
+﻿import SceneB from './scenes/sceneB.js'
+import SceneC from './scenes/sceneC.js'
+
+class OpeningScreen extends Phaser.Scene { /******** OPENING SCREEN ********/
     constructor() {
-        super({ key: 'OpeningScreen' });
+        super({ key: 'OpeningScreen' })
+    }
+
+    init(data) {
+        this.gameData = data.gameData
     }
 
     preload() {
@@ -14,211 +21,168 @@
         var n2 = this.add.text(180, 265, 'Take our professional diagnostics test to discover your personality traits', { font: '12px Courier', fill: '#0f0' })
         var n3 = this.add.text(180, 280, 'and see what strengths and weaknesses you possess.', { font: '12px Courier', fill: '#0f0' })
 
-        var text = this.add.text(250, 345, 'click to begin game', { font: '16px Courier', fill: '#0f0' });
+        var text = this.add.text(250, 345, 'click to begin game', { font: '16px Courier', fill: '#0f0' })
         text.setInteractive({ useHandCursor: true })
-        text.on('pointerup', this.clickButton, this);
+        text.on('pointerup', this.clickButton, this)
     }
     clickButton() {
-        this.scene.start('TitleScene');
+        this.scene.start('TitleScene')
     }
 }
 
-
 class TitleScene extends Phaser.Scene { /******** TITLE SCREEN ********/
 
-    // init(data)
-    // {
-    //     createdCharacter = data.createdChar;
-    //     player = data.playerNew;
-
-    // }
+    init(data)
+    {
+        this.player = data.player
+    }
 
     constructor() {
-        super({ key: 'TitleScene' });
-        // var player;
-        // var createdCharacter;
+        super({ key: 'TitleScene' })
+        this.player
+        // var createdCharacter
     }
 
     preload() {
-        this.load.image('face', './assets/smileyFace.png');
-        this.load.image('icon', './assets/mario.png');
-        this.load.image('pig', './assets/pig.png');
+        this.load.image('face', './assets/smileyFace.png')
+        this.load.image('icon', './assets/mario.png')
+        this.load.image('pig', './assets/pig.png')
     }
 
     create() {
-        var createdCharacter = true;
+        var createdCharacter = true
 
-        var text = this.add.text(250, 300, 'click to begin game', { font: '16px Courier', fill: '#0f0' });
+        var text = this.add.text(250, 300, 'click to begin game', { font: '16px Courier', fill: '#0f0' })
         // text.setInteractive({ useHandCursor: true }).on('pointerover', () => this.enterButtonHoverState() )
-        // .on('pointerout', () => this.enterButtonRestState() ).on('pointerdown', () => this.clickButton2());
+        // .on('pointerout', () => this.enterButtonRestState() ).on('pointerdown', () => this.clickButton2())
 
-        var createFig = this.add.text(250, 400, 'click to create your character', { font: '16px Courier', fill: '#0f0' });
-        //createFig.setInteractive({ useHandCursor: true });
-        var face = this.add.image(210, 300, 'pig');
-        face.setScale(0.04);
-        face.setInteractive({ useHandCursor: true });
+        var createFig = this.add.text(250, 400, 'click to create your character', { font: '16px Courier', fill: '#0f0' })
+        //createFig.setInteractive({ useHandCursor: true })
+        var face = this.add.image(210, 300, 'pig')
+        face.setScale(0.04)
+        face.setInteractive({ useHandCursor: true })
         if (createdCharacter == true) {
-            face.on('pointerup', this.clickButton, this);
+            face.on('pointerup', this.clickButton, this)
         }
         else {
-            face.on('pointerup', this.enterButtonHoverState, this);
+            face.on('pointerup', this.enterButtonHoverState, this)
         }
 
-        var icon = this.add.image(210, 400, 'icon');
-        icon.setScale(0.1);
-        icon.setInteractive({ useHandCursor: true });
-        icon.on('pointerup', this.clickButton2, this);
-
-
+        var icon = this.add.image(210, 400, 'icon')
+        icon.setScale(0.1)
+        icon.setInteractive({ useHandCursor: true })
+        icon.on('pointerup', this.clickButton2, this)
     }
 
     clickButton() {
-        this.scene.start('sceneB');
+        this.scene.start('sceneB', {player: this.player})
     }
 
     clickButton2() {
-        this.scene.start('character');
+        this.scene.start('character')
     }
 
     enterButtonHoverState() {
-
-        this.clickButton.setStyle({ fill: '#ff0' });
-
+        this.clickButton.setStyle({ fill: '#ff0' })
         if (createdCharacter == false) {
-            var popup = this.add.text(265, 275, 'Sorry, please create a character before playing the game');
+            var popup = this.add.text(265, 275, 'Sorry, please create a character before playing the game')
         }
     }
 
     enterButtonRestState() {
-        this.clickButton.setStyle({ fill: '#0f0' });
+        this.clickButton.setStyle({ fill: '#0f0' })
     }
 }
 
 class Character extends Phaser.Scene /******** CHARACTER SCREEN ********/ {
+    
     init() {
-        //var createdCharacter = true;
+        //var createdCharacter = true
     }
-    constructor() {
-        super({ key: 'character' });
 
+    constructor() {
+        super({ key: 'character' })
+        this.player
+        this.arr = [] // push all characters into array and manually set player 
     }
 
     preload() {
-        this.load.image('character', './assets/pig.png');
-        this.load.image('c2', './assets/smileyFace.png');
+        this.load.image('character', './assets/pig.png')
+        this.load.image('c2', './assets/smileyFace.png')
         this.load.image('background', './assets/whiteRectangle.png')
     }
 
-
     create() {
-        var player;
-        let arr = [];
-        var p1C = true;
-        var p2C = true;
-        var p3C = true;  //push all characters into array and manually set player 
-        var p4C = true;
+        var bg1 = this.add.image(100, 90, 'background')
+        bg1.setScale(0.8)
+        var bg2 = this.add.image(300, 90, 'background')
+        bg2.setScale(0.8)
 
-        var bg1 = this.add.image(100, 90, 'background');
-        bg1.setScale(0.8);
+        // CHARACTER OPTION 1
+        var p1 = this.add.sprite(90, 90, 'character')
+        this.addNewPlayer(p1, 0.06)
 
-        var bg2 = this.add.image(375, 90, 'background');
-        bg2.setScale(0.8);
+        // CHARACTER OPTION 2
+        var p2 = this.add.sprite(300, 90, 'c2')
+        this.addNewPlayer(p2, 0.1)
 
+        // RETURN TO MAIN SCREEN
+        var text = this.add.text(250, 400, 'click to return back to menu screen', { font: '16px Courier', fill: '#ffffff' })
+        text.setInteractive({ useHandCursor: true })
+        text.setInteractive()
+        text.on('pointerup', this.clickButton, this)
 
-        var p1 = this.add.sprite(90, 90, 'character');
-        p1.setInteractive({ useHandCursor: true });
-        p1.alpha = 0.4;
-        arr.push(p1);
-        p1.setScale(0.06);
-        p1.inputEnabled = true;
-
-        if (p1C == true) {
-            p1.on('pointerover', function () { p1.alpha = 1; }, this);
-            p1.on('pointerout', function () { p1.alpha = 0.4; }, this);
-        }
-
-        p1.on('pointerup', this.setPlayer1, this);
-        // p1.on('pointerdown', this.hover, this);
-
-
-        var p2 = this.add.sprite(375, 90, 'c2');
-        p2.setInteractive({ useHandCursor: true });
-        p2.alpha = 0.2;
-        arr.push(p2);
-        p2.setScale(0.1);
-        p2.inputEnabled = true;
-
-        if (p2C == true) {
-            p2.on('pointerover', function () { p2.alpha = 1; }, this);
-            p2.on('pointerout', function () { p2.alpha = 0.5; }, this);
-        }
-
-        p2.on('pointerup', this.setPlayer2, this);
-
-        var text = this.add.text(250, 400, 'click to return back to menu screen', { font: '16px Courier', fill: '#ffffff' });
-        text.setInteractive({ useHandCursor: true });
-        text.setInteractive();
-        text.on('pointerup', this.clickButton, this);
-
-        this.input.manager.enabled = true;
+        this.input.manager.enabled = true
 
         // this.input.once('pointerdown', function () {
-
-        //     this.scene.start('TitleScene');
-
-        // }, this);
+        //     this.scene.start('TitleScene')
+        // }, this)
     }
 
     clickButton() {
-        this.scene.start('TitleScene');
-        //, {createdChar: true, player: player});
-    }
-    setPlayer1() {
-        //player = this.add.sprite('character');
-        // player = arr[0];
-        p1C = false, p2C = true, p3C = true, p4C = true;
+        this.scene.start('TitleScene', { player: this.player })
     }
 
-    setPlayer2() {
-        ///player = this.add.sprite('character');
-        //player = arr[1];
-        p1C = true, p2C = false, p3C = true, p4C = true;
+    addNewPlayer(player, scale) {
+        player.setInteractive({ useHandCursor: true })
+        player.setScale(scale)
+        player.inputEnabled = true
+        player.alpha = 0.4
+        player.on('pointerover', function () { 
+            this.alpha = 1
+            console.log("POINTER OVER!")
+        })
+        player.on('pointerout', function () { this.alpha = 0.4 })
+        // save 'this' to a variable for callbacks
+        var self = this
+        player.on('pointerup', function(){
+            self.setPlayer(this) // 'this' represents the clicked element
+            player.alpha = 1
+        })
+        // SAVE TO CHARACTER ARRAY
+        this.arr.push(player)
     }
 
-    setPlayer3() {
-        //player = this.add.sprite('character');
-        //var playerSet = 
+    setPlayer(player) {
+        this.player = player
+        console.log("Selected character! set player", player)
+        player
+        this.update()
     }
 
-    setPlayer4() {
-        //player = this.add.sprite('character');
-        //var playerSet = 
+    // update alpha depending on selected status
+    update() {      
+        for(var i = 0; i < this.arr.length; i++) {
+            if (this.player != this.arr[i]) {
+                this.arr[i].alpha = 0.4
+            } else {
+                this.arr[i].alpha = 1
+            }
+        }
     }
-
-    update() {
-
-        // 
-        // if (p1.input.pointerOver())
-        // {
-        //     p1.alpha = 1;
-        // }
-        // else
-        // {
-        //     p1.alpha = 0.5;
-        // }
-    }
-
-    // hover()
-    // {
-    //     p1.alpha = 1;
-
-    // }
-
-
 }
 
-import SceneB from './scenes/sceneB.js'
-import SceneC from './scenes/sceneC.js'
+export default Character
 
 var config = {
     type: Phaser.AUTO,
@@ -235,16 +199,20 @@ var config = {
         }
     },
     scene: [OpeningScreen, TitleScene, SceneB, SceneC, Character]
-};
+}
 
-var cursors;
-var playerTest;
+// don't know if we need this class, depends on how complicated the
+// data we want to pass around is
+class gameData {
+    constructor(){
+        // this.playerTest
+        this.scoreLeft = 50
+        this.scoreRight = 50
+    }
+}
 
-var scoreLeft = 50;
-var scoreRight = 50;
+var data = new gameData()
+var game = new Phaser.Game(config)
 
-var game = new Phaser.Game(config);
-//var cursors;
-
-game.scene.start(OpeningScreen);
+game.scene.start(OpeningScreen, {gameData: data})
 
--- PersonalityTest/scenes/sceneB.js
+++ PersonalityTest/scenes/sceneB.js
@@ -1,117 +1,118 @@
+import Character from '../exCode.js'
+
 class SceneB extends Phaser.Scene { /******** GAME #2 ********/
     
     constructor ()
     {
-        super({ key: 'sceneB' });
+        super({ key: 'sceneB' })
+        this.player
+        this.cursors
+    }
+
+    init(data)
+    {
+        this.player = data.player
     }
 
     preload ()
     {
-        this.load.image('arrow', 'assets/arrow.png');
-        this.load.image('character', './assets/pig.png');
-        this.load.image('boxObj', './assets/boxBlock.png');
-        // this.load.tilemap('tmap', './assets/gameMap.csv', null, Phaser.Tilemap.CSV);
-        // this.load.image('tiles', './assets/tiles.png');
-        var timedEvent;
+        this.load.image('arrow', 'assets/arrow.png')
+        this.load.image('character', './assets/pig.png')
+        this.load.image('boxObj', './assets/boxBlock.png')
+        // this.load.tilemap('tmap', './assets/gameMap.csv', null, Phaser.Tilemap.CSV)
+        // this.load.image('tiles', './assets/tiles.png')
+        var timedEvent
     }
 
     create ()
     {
         // refer - https://samme.github.io/phaser-examples-mirror/tilemaps/csv%20map%20collide.html
-        //var map = this.add.tilemap('tmap', 16, 16);
-        // map.addTilesetImage('tiles');
+        //var map = this.add.tilemap('tmap', 16, 16)
+        // map.addTilesetImage('tiles')
 
-        // var layer = map.createLayer(0);
+        // var layer = map.createLayer(0)
 
         // //  Resize the world
-        // layer.resizeWorld();
+        // layer.resizeWorld()
 
         // //  This isn't totally accurate, but it'll do for now
-        // map.setCollisionBetween(54, 83);
+        // map.setCollisionBetween(54, 83)
+
+        // console.log("sceneB imported player", this.player)
+        // create physics player from the imported player data
+        this.player = this.physics.add.sprite(48, 48, this.player.texture.key, 1).setScale(0.1)
+        var box = this.add.image(600,550, 'boxObj').setScale(.2)
+
+        // this.physics.enable(player, Phaser.Physics.ARCADE)
+        // player.body.setSize(10, 14, 2, 1)
+
+        this.cursors = this.input.keyboard.createCursorKeys()
+        this.player.setCollideWorldBounds(true).setBounce(.2)
+
+        var help = this.add.text(16, 16, 'Use arrows to move', { font: '14px Arial', fill: '#ffffff' })
+
+        //playerTest = this.add.sprite(100,100, 'pig').setScale(0.5)
+        this.arrow = this.add.sprite(400, 300, 'arrow').setOrigin(0, 0.5)
+        this.arrow.setScale(.5)
 
 
-        this.player = this.physics.add.sprite(48, 48, 'character', 1).setScale(0.1);
+        this.initialTime = 30
 
-        var box = this.add.image(600,550, 'boxObj').setScale(.2);
-
-        // this.physics.enable(player, Phaser.Physics.ARCADE);
-
-        // player.body.setSize(10, 14, 2, 1);
-
-        cursors = this.input.keyboard.createCursorKeys();
-        this.player.setCollideWorldBounds(true).setBounce(.2);
-
-        var help = this.add.text(16, 16, 'Use arrows to move', { font: '14px Arial', fill: '#ffffff' });
-
-
-
-        //playerTest = this.add.sprite(100,100, 'pig').setScale(0.5);
-        this.arrow = this.add.sprite(400, 300, 'arrow').setOrigin(0, 0.5);
-        this.arrow.setScale(.5);
-
-
-        this.initialTime = 30;
-
-        this.text = this.add.text(300, 16, 'Countdown: '+ this.initialTime, { font: '22px Arial', fill: '#ffffff' });
+        this.text = this.add.text(300, 16, 'Countdown: '+ this.initialTime, { font: '22px Arial', fill: '#ffffff' })
         // + formatTime(this.initialTime)
 
         // Each 1000 ms call onEvent
-        //timedEvent = this.time.addEvent({ delay: 1000, callback: onTimeEvent, callbackScope: this, loop: true });
+        //timedEvent = this.time.addEvent({ delay: 1000, callback: onTimeEvent, callbackScope: this, loop: true })
 
 
         this.input.once('pointerdown', function () {
 
-            this.scene.start('sceneC');
+            this.scene.start('sceneC')
 
-        }, this);
+        }, this)
     }
 
     update ()
     {
-        this.arrow.rotation += 0.01;
-       // game.physics.arcade.collide(player, layer);
-        if (cursors.left.isDown)
+        this.arrow.rotation += 0.01
+       // game.physics.arcade.collide(player, layer)
+        if (this.cursors.left.isDown)
         {
-           this.player.setVelocityX(-160);
-
-            //player.anims.play('left', true);
+           this.player.setVelocityX(-160)
+            //player.anims.play('left', true)
         }
-        else if (cursors.right.isDown)
+        else if (this.cursors.right.isDown)
         {
-            this.player.setVelocityX(160);
-
-            //player.anims.play('right', true);
+            this.player.setVelocityX(160)
+            //player.anims.play('right', true)
         }
         else
         {
-            this.player.setVelocityX(0);
-
-            //player.anims.play('turn');
+            this.player.setVelocityX(0)
+            //player.anims.play('turn')
         }
 
-        if (cursors.up.isDown)
+        if (this.cursors.up.isDown)
         {
-            this.player.setVelocityY(-130);
+            this.player.setVelocityY(-130)
         }
     }
 
     // formatTime(seconds){ //ref - https://phaser.discourse.group/t/countdown-timer/2471/6
     //     // Minutes
-    //     var minutes = Math.floor(seconds/60);
+    //     var minutes = Math.floor(seconds/60)
     //     // Seconds
-    //     var partInSeconds = seconds%60;
+    //     var partInSeconds = seconds%60
     //     // Adds left zeros to seconds
-    //     partInSeconds = partInSeconds.toString().padStart(2,'0');
+    //     partInSeconds = partInSeconds.toString().padStart(2,'0')
     //     // Returns formated time
-    //     return `${minutes}:${partInSeconds}`;
+    //     return `${minutes}:${partInSeconds}`
     // }
-    
     
     ontimeEvent ()
     {
-        this.initialTime -= 1; // One second
-        this.text.setText('Countdown: ' + this.initialTime);
+        this.initialTime -= 1 // One second
+        this.text.setText('Countdown: ' + this.initialTime)
     }
-
 }
 export default SceneB
