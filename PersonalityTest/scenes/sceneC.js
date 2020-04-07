db4f4717f99e0861aa80f3ba7236132f6763b966
Lydia DiBlasio <diblasio@usc.edu>
2020-04-07 15:49:20 -0700

    logic in sceneC is broken

--- PersonalityTest/scenes/sceneC.js
+++ PersonalityTest/scenes/sceneC.js
@@ -9,11 +9,12 @@ class SceneC extends Phaser.Scene { /******** GAME #3 ********/
         this.cursors;
         this.curItem;
         this.inv = false;
+        this.itemBool;
     }
 
     init(data)
     {
-        this.player = data.player;
+        this.player = data.player
     }
 
     preload ()
@@ -26,7 +27,7 @@ class SceneC extends Phaser.Scene { /******** GAME #3 ********/
         this.load.tilemapTiledJSON("dun_map", "../tilesets/castleMap.json");
 
         this.load.image('shoe', '../assets/shoe.png');
-        this.load.image('gun', '../assets/gun.png');
+        this.load.image('gun', '../assets/lazergun.png');
         this.load.image('plush','../assets/plushToy.png')
         this.load.image('cloud', '../assets/cloud.png')
         this.load.image('spider', '../assets/spider.png')
@@ -41,25 +42,32 @@ class SceneC extends Phaser.Scene { /******** GAME #3 ********/
         const worldLayer = map.createStaticLayer("Tile Layer 1", tileset, 0, 0).setScale(3.5)
         worldLayer.setCollisionByProperty({ collides: true })
 
-        this.gun = this.physics.add.image(400, 200, 'gun').setScale(0.05);
-        this.shoe = this.physics.add.image(460, 190, 'shoe').setScale(0.2);
-        this.plush = this.physics.add.image(500, 300, 'plush').setScale(0.1);
+        this.gun = this.physics.add.image(510, 300, 'gun').setScale(0.007);
+        this.shoe = this.physics.add.image(460, 160, 'shoe').setScale(0.2);
+        this.plush = this.physics.add.image(600, 500, 'plush').setScale(0.1);
         this.spider = this.physics.add.image(140, 100, 'spider').setScale(.4);
 
         // create physics player from the imported player data
-        this.player = this.physics.add.sprite(100, 450, this.player, 1).setScale(.12);
+        this.player = this.physics.add.sprite(100, 450, this.player, 1).setScale(.012);
         this.physics.add.collider(this.player, worldLayer)
         this.player.setCollideWorldBounds(true).setBounce(.2)
 
         this.text = this.add.text(700, 200, "ITEM", {fill: '#ffffff'});
+        this.instruction = this.add.text(680, 50, "KILL THE SPIDER", {fill: '#ffffff'})
         this.cloud = this.add.image(735,275, 'cloud').setScale(0.4);
         this.curItem = this.add.text(665,250, 'grab an item', {fill: '#000000'});
+        this.itemBool = false;
+
 
         this.cursors = this.input.keyboard.createCursorKeys();
 
         // this.player = this.physics.add.sprite(100, 450, this.player.texture.key,1).setScale(0.12);
 
-        this.physics.add.overlap(this.player, this.plush, this.choose, null, this)
+        //this.physics.add.overlap(this.player, this.p, this.choose, null, this)
+
+        this.physics.add.overlap(this.player, this.gun, this.choose('lazer pistol'), null, this)
+
+        this.physics.add.overlap(this.player, this.plush, this.choose('plush toy'), null, this)
 
         this.physics.add.overlap(this.player, this.spider, this.kill, null, this)
  
@@ -88,19 +96,32 @@ class SceneC extends Phaser.Scene { /******** GAME #3 ********/
         }
 
         this.player.update()
+        this.plush.update()
+        this.gun.update()
+
+        this.physics.add.overlap(this.player, this.gun, this.choose('lazer pistol'), null, this)
+
+        this.physics.add.overlap(this.player, this.plush, this.choose('plush toy'), null, this)
 
 
     }
 
-    choose()
+    choose(item)
     {
-        this.curItem 
+        this.itemBool = true;
+        this.curItem.setText(item); 
     }
 
     kill()
     {
         //do scoring
-        this.scene.start('CompileResults', {player: this.player});
+        if (this.itemBool == false)
+        {
+            var popup = this.add.text(140, 200, 'Sorry, please create a character before playing the game')
+        }
+        else {
+            this.scene.start('CompileResults', {player: this.player});
+        }
     }
 }
 
